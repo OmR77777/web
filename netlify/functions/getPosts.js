b985c8fa890c4d7a45a402b6506d121313cbfdfb
OmR77777 <benamirayazid49@gmail.com>
2025-09-06 17:34:05 +0100

    Update getPosts.js

--- netlify/functions/getPosts.js
+++ netlify/functions/getPosts.js
@@ -5,7 +5,10 @@ export async function handler(event, context) {
   await client.connect();
 
   try {
-    const result = await client.query("SELECT * FROM posts ORDER BY id DESC");
+    const result = await client.query(
+      "SELECT id, title, content, cover, desc, author, created_at FROM posts ORDER BY id DESC"
+    );
+
     return {
       statusCode: 200,
       body: JSON.stringify({ success: true, posts: result.rows }),
